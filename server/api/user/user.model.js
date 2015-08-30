4668f534dbf418bb82b1fb26b58b97b320d986b6
franco malatacca <francomalatacca@gmail.com>
2015-08-30 07:55:53 +0200

    updated user schema (server)

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -4,10 +4,20 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  email: {type: String, required: true, index: {unique: true}},
+  email: {type: String, required: true, index: {unique: true}, lowercase: true},
   bio: String,
   password: {type: String, required: true, minlength: 6, maxlenght: 32},
-  active: {type: Boolean, default: false}
+  passphrase:  {type: String, required: true},
+  active: {type: Boolean, default: false},
+  /**
+   * User legend
+   * 0 - default registered user
+   * 1 - producer
+   * 2 - administrator
+   * */
+  group: {type: Number, default: 0, min: 0, max: 3},
+  lastUpdate: {type: Date, default: Date.now()},
+  created: {type: Date, default: Date.now()}
 });
 
 UserSchema.pre('save', function(next){
@@ -22,6 +32,11 @@ UserSchema.pre('save', function(next){
 });
 
 UserSchema.methods.comparePassword = function(password, callback) {
+  /* bcrypt check goes here */
+  callback(null, true);
+};
+
+UserSchema.methods.validateUserByPassphrase = function(passphrase, callback) {
   /* bcrypt check goes here */
   callback(null, true);
 };
