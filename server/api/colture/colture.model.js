3210de00fa270d3a89b53228b190e278f2dc0e68
franco malatacca <francomalatacca@gmail.com>
2015-09-04 19:32:22 +0200

    updated colture model

--- server/api/colture/colture.model.js
+++ server/api/colture/colture.model.js
@@ -4,9 +4,18 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var ColtureSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  name: {type: String, required: true, lowercase: true},
+  description: {type: String, required: true, lowercase: true},
+  coordinates: {
+    latitude: {type: Schema.Types.Number},
+    longitude: {type: Schema.Types.Number}
+  },
+  type: {type: String, required: true, lowercase: true},
+  area: {type: Schema.Types.Number},
+  active: {type: Boolean, default: false},
+  lastUpdate: {type: Date, default: Date.now()},
+  created: {type: Date, default: Date.now()},
+  _user: {type: Schema.Types.ObjectId, ref: 'User', required: true}
 });
 
 module.exports = mongoose.model('Colture', ColtureSchema);
